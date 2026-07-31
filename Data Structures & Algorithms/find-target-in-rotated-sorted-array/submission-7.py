954dfc3a2f4a720c5663565bc3250a52438bc255
Reagan Armstrong <48657455+reiganarmstrong@users.noreply.github.com>
2026-07-31 15:47:24 -0400

    Add: find-target-in-rotated-sorted-array - submission-7

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-7.py
@@ -0,0 +1,42 @@
+class Solution:
+
+    # binary search O(logn)
+    # lVal <= mVal <= rVal; normal case
+    # lVal <= mVal >= rVal; l and m on left side, r on i's own side
+    # lVal >= mVal <= rVal; l on left side r and m on right side
+
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            lVal, rVal, mVal = nums[l], nums[r], nums[m]
+            if mVal == target:
+                return m
+            # normal case
+            if lVal <= mVal <= rVal:
+                if mVal < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            # m on left
+            elif lVal <= mVal > rVal:
+                if mVal < target:
+                    l = m + 1
+                elif mVal > target:
+                    if lVal > target:
+                        l = m + 1
+                    else:
+                        r = m - 1
+            # m on right
+            elif lVal >= mVal < rVal:
+                if mVal < target:
+                    if target >= lVal:
+                        r = m - 1
+                    else:
+                        l = m + 1
+                elif mVal > target:
+                    r = m - 1
+        
+        return -1
+
